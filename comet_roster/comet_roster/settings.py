e84ffa3b2d02dea3a4cd3d8d4e67055a1277c475
chung-ejy <chung-ejy@gmail.com>
2022-01-23 21:32:55 -0500

    enabled cors

--- comet_roster/comet_roster/settings.py
+++ comet_roster/comet_roster/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'corsheaders',
     'roster',
     'rest_framework',
     'database',
