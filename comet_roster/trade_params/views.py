663d2f73acedfdb2fd4ab80916301d4872b7170b
chung-ejy <chung-ejy@gmail.com>
2022-01-24 08:54:21 -0500

    no touchie 2

--- comet_roster/trade_params/views.py
+++ comet_roster/trade_params/views.py
@@ -24,7 +24,7 @@ def tradeParamsView(request):
                 trade_params = comet_roster.get_user_trade_params(version,user)
                 trade_params["date"] = pd.to_datetime(trade_params["date"])
                 trade_params.sort_values("date",inplace=True)
-                complete = {f"trade_params":trade_params.to_dict("records")}
+                complete = {f"trade_params":trade_params.to_dict("records")[0]}
             else:
                 complete = {"trade_params":{},"errors":"incorrect key"}
         elif request.method == "DELETE":
